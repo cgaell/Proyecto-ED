31e3e82e19e362e2c4a8fc92d2dfa79f2ee6212f
Gael Castro <gaelcastrolara2006@gmail.com>
2025-09-18 14:43:39 -0600

    GUI uploaded

--- src/FloreriaGUI.java
+++ src/FloreriaGUI.java
@@ -2,11 +2,11 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package com.mycompany.floreriaap;
+
 
 /**
  *
- * @author enrique
+ * 
  */
 import javax.swing.*;
 import javax.swing.border.EmptyBorder;
